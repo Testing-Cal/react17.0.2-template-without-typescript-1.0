492e35669248aee395e4d9b68369d99c72936cf7
Yuvraj Dhakate <ydhakate@calibo.com>
2024-03-04 11:58:40 +0530

    added functinal component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './home';
